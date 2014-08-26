e5d05aaab00ce98bdac0c1f56948f6286f59ffb6
Brian Dooley <briandooley@users.noreply.github.com>
2014-08-26 15:23:40 +0100

    appurl change

--- main.js
+++ main.js
@@ -3,8 +3,10 @@
  * - the return 'callback' method signature is 'callback (error, data)', where 'data' is a JSON object.
 */
 
-// You can find this url in the studio dashboard, just add '/cloud/' to
-var appurl = 'https://support-tbd8odimttbsvkhssiqdij3l-dev.df.dev.e111.feedhenry.net/cloud/'
+// You can find this url in the studio dashboard.
+var appurl = 'https://support-tbd8odimttbsvkhssiqdij3l-dev.df.dev.e111.feedhenry.net';
+appurl = appurl + '/cloud/';
+
 // Detail of this request module can be found at https://github.com/mikeal/request
 var request = require('request');
 
